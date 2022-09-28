be673cf4736b45fc7459dd8ce662c393b5f6cb53
GiangBaoMinh <100124047+GiangBaoMinh@users.noreply.github.com>
2022-09-28 16:05:33 +0700

    Test Commit and Push Intialize stage

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String Args){
+        launch(Args);
+    }
+
+    @Override
+    public void start(Stage theStage){
+        theStage.setTitle("Bomb");
+        theStage.show();
+    }
+}
